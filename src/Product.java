169b46b2fda56c66d8e853f18373a9d1c9d78410
Pranav Raut <Pranavraut033@gmail.com>
2017-10-13 14:21:32 +0530

    Added files

---
+++ src/Product.java
@@ -0,0 +1,34 @@
+package product;
+
+class Product {
+    String name, description;
+    double price;
+    int category;
+    
+    Product(String a,String b,double c,int d){
+        name=a;
+        description=b;
+        price=c;
+        category=d;
+    }
+    public String getName()
+    {
+    	return name;
+
+    }
+    public double getPrice()
+    {
+    	return price;
+    }
+    public int getCategory()
+    {
+    	return category;
+    }
+    public String getDescription()
+    {
+    	return description;
+    }
+
+
+
+ }
